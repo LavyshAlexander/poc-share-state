9d55f629d2850fd3f7128f82d92e8e91d89e5ad3
Lavysh Alexander <LavyshAlexander@gmail.com>
2022-03-15 17:18:29 +0300

    Added Share atom.

---
+++ src/features/form/atoms/Share.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react"
+import { ActionCreatorWithoutPayload } from "@reduxjs/toolkit"
+import { useAppDispatch, useAppSelector } from "../../../app/hooks"
+import { RootState } from "../../../app/store"
+
+
+interface IShare {
+	selector: (s: RootState) => string
+	action: ActionCreatorWithoutPayload
+}
+
+export const Share: FC<IShare> = ({ selector, action }) => {
+	const value = useAppSelector(selector)
+	const dispatch = useAppDispatch()
+
+	return (
+		<div className="form-group">
+			<textarea
+				readOnly
+				id="share"
+				className="form-control"
+				value={value}
+			/>
+
+			<button onClick={() => dispatch(action())}>Share</button>
+		</div>
+	)
+}
